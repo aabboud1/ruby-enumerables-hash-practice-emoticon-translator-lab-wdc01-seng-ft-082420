7cfc3e867e2e14fbbff298b87d8e94520e10f8a0
Ahmed Abboud <abboudahmed1@gmail.com>
2020-08-16 17:44:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,31 +1,48 @@
+require "pry"
 require "yaml"
+emote = YAML.load_file('./lib/emoticons.yml')
+	
 
-def load_library(file_path)
-  emoticons = YAML.load_file(file_path)
+	
 
-  new_hash = {
-    'get_meaning' => {},
-    'get_emoticon' => {}
-  }
+	def load_library(file_path)
+	  emote = YAML.load_file(file_path)
+	  working_hash = {}
+	  emote.each do |(key, emote_array)|
+	  emote_array.each do |x|
+	    working_hash[key] = {}
+	    working_hash[key][:english] = emote_array[0]
+	    working_hash[key][:japanese] = emote_array[1]
+	   end
+	  end 
+  	working_hash
+	end
+	
 
-  emoticons.each do |key,value|
-    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
-    new_hash['get_meaning'][value[1]] = key
-  end
+	def get_japanese_emoticon(file_path, english_emoticon)
+	  working_hash = load_library(file_path)
+	  japanese_emoticon = "Sorry, that emoticon was not found"
+	  working_hash.each do |(key, emote_hash)|
+	    emote_hash.each do |(language_key, emote)|
+	    if emote == english_emoticon
+	      japanese_emoticon = working_hash[key][:japanese]
+	    end
+	  end
+	  end
+	  japanese_emoticon
+	end
+	
 
-  new_hash
-end	
-
-
-def get_japanese_emoticon(file_path, emoticon)
-  # code goes here	  
-  result = load_library(file_path)['get_emoticon'][emoticon]
-  result ? result : "Sorry, that emoticon was not found"
-end	
-
-
-def get_english_meaning(file_path, emoticon)
-  # code goes here	 
-  result = load_library(file_path)['get_meaning'][emoticon]
-  result ? result : "Sorry, that emoticon was not found"
-end 	
+	def get_english_meaning(file_path, emoticon)
+	# code goes here
+	  working_hash = load_library(file_path)
+	  meaning = "Sorry, that emoticon was not found"
+	  working_hash.each do |(key, emote_hash)|
+	    emote_hash.each do |(language_key, emote)|
+    	if emote == emoticon 
+	    meaning = key
+	    end
+  	end
+	end
+	meaning
+	end
